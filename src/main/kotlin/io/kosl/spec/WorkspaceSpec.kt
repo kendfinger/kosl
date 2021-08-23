3db72833a87fca1e0a5bda947e5046631de270b1
Kenneth Endfinger <kaendfinger@gmail.com>
2021-08-22 22:39:52 -0700

    ♻️ Cleanup Imports

--- src/main/kotlin/io/kosl/spec/WorkspaceSpec.kt
+++ src/main/kotlin/io/kosl/spec/WorkspaceSpec.kt
@@ -1,11 +1,7 @@
 package io.kosl.spec
 
-import io.kosl.state.ServiceState
-import io.kosl.state.WorkspaceState
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.serializer
-import java.nio.file.Path
-import java.nio.file.Paths
 
 @Serializable
 class WorkspaceSpec(
