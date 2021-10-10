f67a4d4abf5e807ef9a75a3b5f60568e081c8c02
Kenneth Endfinger <kaendfinger@gmail.com>
2021-10-10 04:19:57 -0400

    Upgrade Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.5.30"
-  kotlin("plugin.serialization") version "1.5.30"
+  kotlin("jvm") version "1.5.31"
+  kotlin("plugin.serialization") version "1.5.31"
 
   id("com.github.johnrengelman.shadow") version "7.0.0"
 
@@ -16,20 +16,22 @@ repositories {
 fun kotlinx(name: String, version: String): String = "org.jetbrains.kotlinx:kotlinx-$name:$version"
 
 dependencies {
-  api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
-  api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.1")
-  api("io.fabric8:kubernetes-client:5.7.2")
+  api("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+
+  @Suppress("GradlePackageUpdate")
+  api("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+  api("io.fabric8:kubernetes-client:5.8.0")
 
   implementation("com.github.ajalt.clikt:clikt:3.2.0")
 
   implementation("org.slf4j:slf4j-api:1.7.32")
   implementation("org.slf4j:slf4j-simple:1.7.32")
 
-  testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
-  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
   testImplementation("org.testcontainers:testcontainers:1.16.0")
   testImplementation("org.testcontainers:junit-jupiter:1.16.0")
-  testImplementation(kotlin("test", "1.5.30"))
+  testImplementation(kotlin("test", "1.5.31"))
 }
 
 tasks.withType<KotlinCompile> {
